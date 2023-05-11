2948ae8b329f4fe43221c9bd7c53b5a33252a6af
Hunterts117 <113431088+Hunterts117@users.noreply.github.com>
2023-05-11 23:50:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 import tempfile
 import streamlit as st
 from streamlit_chat import message
-from agent import Agent
+from NomuraGPT import NomuraGPT
 
 st.set_page_config(page_title="NomuraGPT")
-
+os.environ['OPENAI_API_KEY'] = "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"
 
 def display_messages():
     st.subheader("NomuraGPT")
@@ -28,44 +28,16 @@ def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
         st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
-        if is_openai_api_key_set():
-            st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
-        else:
-            st.session_state["agent"] = None
+
+        st.session_state["agent"] = NomuraGPT(st.session_state["OPENAI_API_KEY"])
 
     st.header("NomuraGPT")
 
-    if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
-        if (
-            len(st.session_state["input_OPENAI_API_KEY"]) > 0
-            and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
-        ):
-            st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
-            if st.session_state["agent"] is not None:
-                st.warning("Please, upload the files again.")
-            st.session_state["messages"] = []
-            st.session_state["user_input"] = ""
-            st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
-
-    st.subheader("Upload a document")
-    st.file_uploader(
-        "Upload document",
-        type=["pdf"],
-        key="file_uploader",
-        on_change=read_and_save_file,
-        label_visibility="collapsed",
-        accept_multiple_files=True,
-        disabled=not is_openai_api_key_set(),
-    )
-
-    st.session_state["ingestion_spinner"] = st.empty()
-
     display_messages()
-    st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+    
+    st.text_input("Message", key="user_input", on_change=process_input)
 
     st.divider()
-    st.markdown("Source code: [Github](https://github.com/viniciusarruda/chatpdf)")
-
 
 if __name__ == "__main__":
     main()
